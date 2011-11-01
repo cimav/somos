61cb6776bf5ef7bdb08b06674abe66e30e004c66
Jonathan Hernández <ion@suavizado.com>
2011-11-01 13:29:29 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Somos::Application.config.secret_token = '5615c145d1447ead0c24553be2cf16e93ac4fa069569d36ed4586cb1f1b44f3f00d960b820b8fa7aa1fae168192d23e26340334161fac70ca42763ddf09345d2'
