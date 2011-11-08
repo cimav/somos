274227d279480ce7181ac15d679e9e4b94b56c11
Jonathan Hernández <ion@suavizado.com>
2011-11-08 11:53:11 -0700

    Add birth_date

--- db/migrate/20111105004009_create_users.rb
+++ db/migrate/20111105004009_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string     "first_name", :null => false
       t.string     "last_name",  :null => false
       t.string     "email",      :null => false
+      t.date       "birth_date"
       t.text       "bio"
       t.string     "image"
       t.integer    "reports_to"
