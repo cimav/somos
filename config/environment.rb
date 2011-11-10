6c33bfb1ce3004adb55f1f9d953f9c061686abc5
Jonathan Hernández <ion@suavizado.com>
2011-11-09 17:58:30 -0700

    Add Google Apps configuration

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,9 @@
+# Config
+GAPPS_DOMAIN = 'your-google-apps-domain.com'
+GAPPS_CONSUMER_KEY = GAPPS_DOMAIN
+GAPPS_CONSUMER_SECRET = 'SECRET-KEY'
+GAPPS_REQUESTOR_ID = 'admin-user@your-google-apps-domain.com'
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
