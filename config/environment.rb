d44528ead3d25834ada9f4ebb9178cb969218a13
Jonathan Hernández <ion@suavizado.com>
2012-03-02 11:42:35 -0700

    New constant

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,13 @@
 # Config
-GAPPS_DOMAIN = 'your_google_apps_domain.com'
+GAPPS_DOMAIN = 'your-domain.com'
 GAPPS_CONSUMER_KEY = GAPPS_DOMAIN
-GAPPS_CONSUMER_SECRET = 'YOUR_KEY_SECRET'
-GAPPS_REQUESTOR_ID = 'some-admin-of@your_google_apps_domain.com'
+GAPPS_CONSUMER_SECRET = 'secret'
+GAPPS_REQUESTOR_ID = 'admin@your-domain.com'
 
-HOME_INITIAL_POSTS = 10
+HOME_INITIAL_POSTS = 5
+
+SITE_NAME = 'SOMOS'
+
 
 # Load the rails application
 require File.expand_path('../application', __FILE__)
