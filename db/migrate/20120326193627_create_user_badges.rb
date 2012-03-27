57e2bc4521f1de80d72145f139360a3752000f81
Jonathan Hernández <ion@suavizado.com>
2012-03-27 16:03:44 -0700

    User badge table

---
+++ db/migrate/20120326193627_create_user_badges.rb
@@ -0,0 +1,14 @@
+class CreateUserBadges < ActiveRecord::Migration
+  def change
+    create_table :user_badges do |t|
+      t.references :user
+      t.references :badge
+      t.references :post
+      t.integer    :status, :default => 1
+      t.timestamps
+    end
+    add_index(:user_badges, :user_id)
+    add_index(:user_badges, :badge_id)
+    add_index(:user_badges, :post_id)
+  end
+end
