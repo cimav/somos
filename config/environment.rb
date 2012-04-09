e8070abbb12ed0a3528f5e23adbbd0e07d4021a5
Jonathan Hernández <ion@suavizado.com>
2012-04-09 17:47:45 -0600

    Remove CONSTANTS, now configuration is done via ENV vars

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,3 @@
-# Config
-GAPPS_DOMAIN = 'your-domain.com'
-GAPPS_CONSUMER_KEY = GAPPS_DOMAIN
-GAPPS_CONSUMER_SECRET = 'secret'
-GAPPS_REQUESTOR_ID = 'admin@your-domain.com'
-
-HOME_INITIAL_POSTS = 5
-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
