ff168c594d43defbb499555b1603908aefcea0c4
Jonathan Hernández <ion@suavizado.com>
2012-04-09 18:10:47 -0600

    Ooops

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,5 @@
 #require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  puts ENV['GOOGLE_KEY']
-  puts "Chido"
-  puts ENV.inspect
   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {approval_prompt: ''}
 end
