2adfca2eac9534335c69e58f74b5283e6ecd8ad5
Jonathan Hernández <ion@suavizado.com>
2012-07-06 15:55:01 -0600

    Display only active events

--- app/controllers/post_events_controller.rb
+++ app/controllers/post_events_controller.rb
@@ -2,7 +2,7 @@ class PostEventsController < ApplicationController
   before_filter :auth_required
   respond_to :html, :json
   def upcoming
-    @events = PostEvent.where("(start_date >= :date) OR (end_date >= :date)", {:date => Time.now.strftime("%Y-%m-%d")}).limit(10).order(:start_date)
+    @events = PostEvent.includes(:post).where("posts.status = :status AND ((start_date >= :date) OR (end_date >= :date))", {:status => Post::ACTIVE,  :date => Time.now.strftime("%Y-%m-%d")}).limit(10).order(:start_date)
     if @events.count > 0
       render :layout => false
     else
