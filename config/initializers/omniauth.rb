ec4bde588e9defafed3bc8fd2742c294c0e2be5f
Jonathan Hernández <ion@suavizado.com>
2012-08-15 18:50:11 -0600

    Don't prompt

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-#require 'openid/store/filesystem'
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {access_type: 'online', approval_prompt: ''}
 end
