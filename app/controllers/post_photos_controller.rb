8e5bb5e6ce2d9b08dc4c648b854265fd8d703e21
Jonathan Hernández <ion@suavizado.com>
2012-11-09 15:58:58 -0700

    Download images link

--- app/controllers/post_photos_controller.rb
+++ app/controllers/post_photos_controller.rb
@@ -1,7 +1,11 @@
 class PostPhotosController < ApplicationController
   def photo
     post = Post.find(params[:id])
-    post_photo = post.post_photo.find(params[:photo_id]).photo_url(params[:version]).to_s
-    send_file post_photo.to_s, :x_sendfile=>true
+    if params[:version] == 'original'
+      post_photo = post.post_photo.find(params[:photo_id]).photo_url.to_s
+    else
+      post_photo = post.post_photo.find(params[:photo_id]).photo_url(params[:version]).to_s
+    end
+    send_file post_photo.to_s, :x_sendfile=>true, :disposition => 'inline'
   end
 end
